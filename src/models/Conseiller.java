ad60b19fff76f1f2660a2c98efd53caad2783dc8
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-09-22 16:49:00 +0100

    Créer la classe fille Conseiller

---
+++ src/models/Conseiller.java
@@ -0,0 +1,10 @@
+package models;
+
+public class Conseiller extends Person {
+
+    public Conseiller() { super(); }
+
+    public Conseiller(String nom, String prenom, String email) {
+        super(nom, prenom, email);
+    }
+}

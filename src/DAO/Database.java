6d558ec1291e48f9f60f22c0e0762f44ac8c6a84
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-09-23 12:22:00 +0100

    Initialisation de la connexion à la base MySQL pour le DAO

---
+++ src/DAO/Database.java
@@ -0,0 +1,24 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Database {
+    private static Connection connection = null;
+
+    private static final String URL = "jdbc:mysql://localhost:3306/gestion_assurance";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    static {
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() {
+        return connection;
+    }
+}

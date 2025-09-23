6a2bab8658f40392ee50fcf11c09cc565901b53b
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-09-23 17:00:00 +0100

    Créer la classe SinistreDAO avec la methode addSinistre

---
+++ src/DAO/SinistreDAO.java
@@ -0,0 +1,31 @@
+package DAO;
+
+import models.Sinistre;
+import models.TypeSinistre;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SinistreDAO {
+    static Connection con = Database.getConnection();
+
+    public void addSinistre(Sinistre sinistre) throws Exception {
+        String sql = "INSERT INTO sinistres (id, type_sinistre, date, cout, description, contrat_id) VALUES (?, ?, ?, ?, ?, ?)";
+
+        PreparedStatement p = con.prepareStatement(sql);
+        p.setString(1, sinistre.getId());
+        p.setObject(2, sinistre.getTypeSinistre().name());
+        p.setObject(3, Timestamp.valueOf(sinistre.getDate()));
+        p.setDouble(4, sinistre.getCout());
+        p.setString(5, sinistre.getDescription());
+        p.setObject(6, sinistre.getContrat());
+
+        p.executeUpdate();
+    }
+
+}

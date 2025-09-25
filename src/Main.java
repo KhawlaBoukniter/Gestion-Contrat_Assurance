e39d8f37de02c5a662548d8ab61b71e11dced618
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-09-25 21:42:00 +0100

    Création de la classe Main pour lancer l'application

--- src/Main.java
+++ src/Main.java
@@ -1,55 +1,40 @@
-import views.ClientView;
-import views.ContratView;
-import views.SinistreView;
-import views.ConseillerView;
+import DAO.ClientDAO;
+import DAO.ConseillerDAO;
+import DAO.ContratDAO;
+import DAO.SinistreDAO;
+import services.ClientService;
+import services.ConseillerService;
+import services.ContratService;
+import services.SinistreService;
+import views.*;
 
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
+        try {
+            ConseillerDAO conseillerDAO = new ConseillerDAO();
+            ClientDAO clientDAO = new ClientDAO();
+            ContratDAO contratDAO = new ContratDAO();
+            SinistreDAO sinistreDAO = new SinistreDAO();
 
-        ClientView clientView = new ClientView();
-        ContratView contratView = new ContratView();
-        SinistreView sinistreView = new SinistreView();
-        ConseillerView conseillerView = new ConseillerView();
+            ConseillerService conseillerService = new ConseillerService(conseillerDAO);
+            ClientService clientService = new ClientService(clientDAO);
+            ContratService contratService = new ContratService(contratDAO);
+            SinistreService sinistreService = new SinistreService(sinistreDAO);
 
-        int choix;
-        do {
-            System.out.println("\nMenu Principal");
-            System.out.println("1. Gérer les conseillers");
-            System.out.println("2. Gérer les clients");
-            System.out.println("3. Gérer les contrats");
-            System.out.println("4. Gérer les sinistres");
-            System.out.println("0. Quitter");
-            System.out.print("Votre choix : ");
-            choix = sc.nextInt();
-            sc.nextLine();
+            MainMenu menu = new MainMenu(
+                    conseillerService,
+                    clientService,
+                    contratService,
+                    sinistreService
+            );
+            menu.afficherMenu();
 
-            switch (choix) {
-                case 1:
-                    conseillerView.menuConseiller();
-                    break;
-                case 2:
-                    clientView.menuClient();
-                    break;
-                case 3:
-                    contratView.menuContrat();
-                    break;
-                case 4:
-                    sinistreView.menuSinistre();
-                    break;
-                case 0:
-                    System.out.println("Sortie de l'application...");
-                    break;
-                default:
-                    System.out.println("Choix invalide, réessayez.");
-                    break;
-            }
-
-        } while (choix != 0);
-
-        sc.close();
+        } catch (Exception e) {
+            System.err.println("Erreur lors du démarrage de l’application : " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }

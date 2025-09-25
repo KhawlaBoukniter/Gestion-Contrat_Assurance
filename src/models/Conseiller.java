5867eb190311843b924b52e7254836d389874fcd
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-09-25 17:02:00 +0100

    Ajouter la methode toString

--- src/models/Conseiller.java
+++ src/models/Conseiller.java
@@ -7,4 +7,8 @@ public class Conseiller extends Person {
     public Conseiller(String nom, String prenom, String email) {
         super(nom, prenom, email);
     }
+
+    public String toString() {
+        return super.toString();
+    }
 }

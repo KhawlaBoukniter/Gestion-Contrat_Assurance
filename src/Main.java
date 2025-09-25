d4da1ac300fb7d534ff88ee35d20f46fbeeb0b76
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-09-25 10:12:00 +0100

    Ajout du fichier Main pour le menu principal de l’application

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,55 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import views.ClientView;
+import views.ContratView;
+import views.SinistreView;
+import views.ConseillerView;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        ClientView clientView = new ClientView();
+        ContratView contratView = new ContratView();
+        SinistreView sinistreView = new SinistreView();
+        ConseillerView conseillerView = new ConseillerView();
+
+        int choix;
+        do {
+            System.out.println("\nMenu Principal");
+            System.out.println("1. Gérer les conseillers");
+            System.out.println("2. Gérer les clients");
+            System.out.println("3. Gérer les contrats");
+            System.out.println("4. Gérer les sinistres");
+            System.out.println("0. Quitter");
+            System.out.print("Votre choix : ");
+            choix = sc.nextInt();
+            sc.nextLine();
+
+            switch (choix) {
+                case 1:
+                    conseillerView.menuConseiller();
+                    break;
+                case 2:
+                    clientView.menuClient();
+                    break;
+                case 3:
+                    contratView.menuContrat();
+                    break;
+                case 4:
+                    sinistreView.menuSinistre();
+                    break;
+                case 0:
+                    System.out.println("Sortie de l'application...");
+                    break;
+                default:
+                    System.out.println("Choix invalide, réessayez.");
+                    break;
+            }
+
+        } while (choix != 0);
+
+        sc.close();
     }
 }

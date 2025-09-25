62aac53f9d6e4a960e9f8dc7a878c61b939c1ac6
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-09-25 19:58:00 +0100

    Ajouter le menu de gestion de sinistres

---
+++ src/views/SinistreView.java
@@ -0,0 +1,177 @@
+package views;
+
+import models.Sinistre;
+import enums.TypeSinistre;
+import services.SinistreService;
+
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
+public class SinistreView {
+    private SinistreService sinistreService = new SinistreService();
+    private Scanner sc = new Scanner(System.in);
+
+    public SinistreView (SinistreService sinistreService) {
+        this.sinistreService = sinistreService;
+    }
+
+    public SinistreView() {}
+
+    public void menuSinistre() throws Exception {
+        int choix;
+        do {
+            System.out.println("\nGerer les Sinistres");
+            System.out.println("1. Ajouter un sinistre");
+            System.out.println("2. Supprimer un sinistre");
+            System.out.println("3. Rechercher un Sinistre par ID");
+            System.out.println("4. Calculer les couts totaux des sinistres d’un client");
+            System.out.println("5. Afficher les sinistres d’un contrat");
+            System.out.println("6. Afficher les sinistres triés par montant decroissant");
+            System.out.println("7. Afficher les sinistres par l’id d’un client");
+            System.out.println("8. Afficher les sinistres qui se sont produits avant une date donnée");
+            System.out.println("9. Afficher les sinistres dont le cout est superieur a un montant donné");
+            System.out.println("0. Retour au menu précédent");
+
+            choix = sc.nextInt();
+            sc.nextLine();
+
+            switch (choix) {
+                case 1:
+                    ajouterSinistre();
+                    break;
+                case 2:
+                    supprimerSinistre();
+                    break;
+                case 3:
+                    rechercherSinistre();
+                    break;
+                case 4:
+                    calculerCoutTotal();
+                    break;
+                case 5:
+                    afficherParContrat();
+                    break;
+                case 6:
+                    afficherTriesParMontant();
+                    break;
+                case 7:
+                    afficherParClient();
+                    break;
+                case 8:
+                    afficherAvantDate();
+                    break;
+                case 9:
+                    afficherCoutSuperieur();
+                    break;
+                case 0:
+                    System.out.println("Retour...");
+                    break;
+                default:
+                    System.out.println("Choix invalide");
+                    break;
+            }
+        } while (choix != 0);
+    }
+
+    public TypeSinistre typeSinistre() {
+        int choix;
+
+        System.out.println("\nChoisissez le type du sinistre");
+        System.out.println("1. Accident de voiture");
+        System.out.println("2. Accident de maison");
+        System.out.println("3. Maladie");
+
+        choix = sc.nextInt();
+        sc.nextLine();
+
+        TypeSinistre type = null;
+
+        switch (choix) {
+            case 1:
+                type = TypeSinistre.ACCIDENT_VOITURE;
+                break;
+            case 2:
+                type = TypeSinistre.ACCIDENT_MAISON;
+                break;
+            case 3:
+                type = TypeSinistre.MALADIE;
+                break;
+            default:
+                System.out.println("Choix invalide");
+                break;
+        }
+        return type;
+    }
+
+    public void ajouterSinistre() throws Exception {
+        TypeSinistre type = typeSinistre();
+        System.out.print("Description : ");
+        String description = sc.nextLine();
+        System.out.print("Cout : ");
+        Double cout = sc.nextDouble();
+        sc.nextLine();
+
+        System.out.print("ID du contrat associé : ");
+        String contratId = sc.nextLine();
+
+        Sinistre s = new Sinistre(LocalDateTime.now(), cout, description, type, contratId);
+        sinistreService.addSinistre(s);
+        System.out.println("Sinistre ajouté avec ID : " + s.getId());
+    }
+
+    private void supprimerSinistre() throws Exception {
+        System.out.print("ID du sinistre à supprimer : ");
+        String id = sc.nextLine();
+        sinistreService.deleteById(id);
+    }
+
+    private void rechercherSinistre() throws Exception {
+        System.out.print("ID du sinistre : ");
+        String id = sc.nextLine();
+        sinistreService.getById(id)
+                .ifPresent(
+                        s -> System.out.println("Trouvé : " + s)
+                );
+    }
+
+    private void afficherParContrat() throws Exception {
+        System.out.print("ID du contrat : ");
+        String contratId = sc.nextLine();
+        sinistreService.getByContrat(contratId)
+                .forEach(System.out::println);
+    }
+
+    private void afficherParClient() throws Exception {
+        System.out.print("ID du client : ");
+        String clientId = sc.nextLine();
+        sinistreService.getSinistresByClientId(clientId)
+                .forEach(System.out::println);
+    }
+
+    private void afficherAvantDate() throws Exception {
+        System.out.print("Date limite (yyyy-MM-ddTHH:mm) : ");
+        LocalDateTime date = LocalDateTime.parse(sc.nextLine());
+        sinistreService.getBeforeDate(date)
+                .forEach(System.out::println);
+    }
+
+    private void afficherCoutSuperieur() throws Exception {
+        System.out.print("Montant minimum : ");
+        double montant = sc.nextDouble();
+        sc.nextLine();
+        sinistreService.getByCoutGreaterThan(montant)
+                .forEach(System.out::println);
+    }
+
+    private void afficherTriesParMontant() throws Exception {
+        sinistreService.getSortedByCoutDesc()
+                .forEach(System.out::println);
+    }
+
+    private void calculerCoutTotal() throws Exception {
+        System.out.print("ID du client : ");
+        String clientId = sc.nextLine();
+        double total = sinistreService.calculateTotalCostByClientId(clientId);
+        System.out.println("Cout total = " + total);
+    }
+}
